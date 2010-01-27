23d40176f3f509cfbe164649090cc3cdc9dfc1e8
Torjus <nifty02@gmail.com>
2010-01-27 14:33:08 +0100

    - Fixed issue where subnav wouldn't slide down if you moved your cursor from a item without children, to one with children.

--- jquery.funkyMenu.js
+++ jquery.funkyMenu.js
@@ -7,7 +7,9 @@
 				$subAnchors = $lists.find('li'),
 				$subNavs = $('ul', $lists),
 				$subHovers = $subAnchors.children('span');
-				
+			
+			var runOnce = false;
+						
 			$nav.after('<span id="bg-easer" />');
 			var $easer = $('#bg-easer');			
 			if ($lists.filter('.tempActive').length > 0) {
@@ -15,6 +17,7 @@
 					width : parseInt($lists.filter('.tempActive').width()),
 					left : parseInt($lists.filter('.tempActive').offset().left) - parseInt($nav.offset().left)
 				}, 500, 'linear', function () {
+					ready = true;
 					$lists.filter('.tempActive').children('ul').slideDown('fast');
 				});
 			}
@@ -32,18 +35,25 @@
 				var $this = $(this),
 					$sub = $this.siblings('ul:first'),
 					$tempActive = $lists.filter('.tempActive'),
-					doSlide = $tempActive.length;
-					
+					doSlide =  $tempActive.length;
+				
+				// We only wan't to run this once
+				// After hover event is fired, there should no longer be a mix up
+				// between .tempActive and .childless-tempActive
+				if (doSlide > 0 && $tempActive.children('ul').length === 0 && !runOnce) {
+					doSlide = 0;
+					runOnce = true;
+				}				
+				
 				$this.addClass('current');
 				$lists.removeClass('childless-tempActive');
 				$subHovers.hide();
-								
+				
 				$easer.stop().animate({
 					width: parseInt($this.outerWidth()),
 					left: parseInt($this.offset().left) - parseInt($nav.offset().left)
 				});
-				
-				if (!$this.parent().hasClass('tempActive')) {
+				if (!$this.parent().hasClass('tempActive') || $this.next('ul').length === 0) {
 					if (doSlide > 0) {
 						$subNavs.stop(true, true);
 						$lists.filter('.tempActive').children('ul').slideUp('fast', function () {
@@ -53,8 +63,8 @@
 						$sub.slideDown('fast');
 					}
 					$lists.removeClass('tempActive');
-				}	
-			}, function () {							
+				}
+			}, function () {
 				if ($(this).siblings('ul').length > 0) {
 					$(this).parent().addClass('tempActive');
 				} else {
